f3601e4be9e352f9175efaa201c1787b1797082a
hesoru <95680931+hesoru@users.noreply.github.com>
2022-09-06 17:24:01 -0700

    Update 1_Data_Import_and_ChIP-seq.sh

--- manuscript/bash/1_Data_Import_and_ChIP-seq.sh
+++ manuscript/bash/1_Data_Import_and_ChIP-seq.sh
@@ -2,7 +2,6 @@
 
 #Sunset Enhancers: Tracing H3K27 Acetylation on Closed Chromatin in Myeloid Lineage Differentiation
 #Authors: Melanie Law, Helena Sokolovska, Andrew Murtha, Kitoosepe Martens, Annice Li, and Kalen Dofher
-#Date: Sept 5, 2022
 
 ##### DATA IMPORT ####
 
